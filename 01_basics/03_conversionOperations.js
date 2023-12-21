ffb10060e38c8b9becef8ad99da751a8f58fc69d
Ajay Anuragi <ajayanuragi44@gmail.com>
2023-12-21 09:50:54 +0530

    added operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+let score = "33";
+let valueInNumber  = Number(score);
+
+// console.log(typeof(score)) 
+// console.log(typeof(valueInNumber)) // Nan
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1
+
+
+let isLoggedIn = true;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0=>false
+// "" => false;
+// "ajay" => true;
+
+let marks = 11;
+let stringMarks = String(marks);
+
+// console.log(typeof stringMarks);
+
+//  OPERATIONS
+
+console.log("1" + 2) // 12
+console.log(1 + "2") //12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); //32
