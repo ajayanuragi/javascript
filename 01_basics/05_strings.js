caea6b34e27d976680834f29871de28961ad7016
Ajay Anuragi <ajayanuragi44@gmail.com>
2023-12-22 18:14:39 +0530

    added string.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = "Ajay";
+let repoCount = 21;
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const newName = new String("Ajay Anuragi")
+// console.log(newName.length);
+// console.log(newName.toUpperCase)
+console.log(newName.indexOf('j'));
+//substring (0, 4) => start from 0 and don't include 4th
+// slice => you can give -ve values
+
+const weirdName = "    Ajay  "
+console.log(weirdName);
+console.log(weirdName.trim());
+console.log(weirdName.replace(" ", "%20"))
