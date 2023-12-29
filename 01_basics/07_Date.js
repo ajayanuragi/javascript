03c8accc7e1945758860997231c35832167054d5
Ajay Anuragi <ajayanuragi44@gmail.com>
2023-12-29 14:17:26 +0530

    edit date.js

--- 01_basics/07_Date.js
+++ 01_basics/07_Date.js
@@ -3,3 +3,7 @@ console.log(date.toString());
 let myTimeStamp = Date.now();
 console.log(myTimeStamp);// in mili seconds
 console.log(Math.floor(myTimeStamp/1000)) // in seconds
+console.log(date.toLocaleString("default",{
+    weekday:"long",
+    
+}))
